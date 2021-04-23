4a462623438220cb4ddc1afcd65d7535c6c95f2b
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2021-04-23 12:28:05 +0200

    Applying feedback

--- src/javascript/app/pages/cashier/payment_agent_list.js
+++ src/javascript/app/pages/cashier/payment_agent_list.js
@@ -96,7 +96,7 @@ const PaymentAgentList = (() => {
 
     const showEmptyListMsg = () => {
         $('.barspinner').setVisibility(0);
-        getElementById('no_paymentagent').setVisibility(1);
+        $('#no_paymentagent').setVisibility(1);
     };
 
     return {
