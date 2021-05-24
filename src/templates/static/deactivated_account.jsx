ca894225194fe35a48f519041c2e009b098e304a
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2021-05-24 16:02:02 +0200

    removing unused class name

--- src/templates/static/deactivated_account.jsx
+++ src/templates/static/deactivated_account.jsx
@@ -4,7 +4,7 @@ const DeactivatedAccount = () => (
     <div id='msg_main' className='center-text gr-gutter gr-padding-30 close-user-account'>
         <div className='msg_main_modal'>
             <div className='notice-msg'>
-                <img className='responsive lock-deactivated-account' src={it.url_for('images/pages/deactivated_account/lock.svg')} alt='Lock image' />
+                <img className='responsive' src={it.url_for('images/pages/deactivated_account/lock.svg')} alt='Lock image' />
                 <h1>
                     {it.L('We\'re sorry to see you leave.')}
                 </h1>
