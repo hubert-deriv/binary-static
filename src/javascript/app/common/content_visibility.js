119d9b9baf380a91bb3d3db4fe4fb65200d7cf76
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2021-06-02 14:40:20 +0200

    centering text only on real account creation mobile

--- src/javascript/app/common/content_visibility.js
+++ src/javascript/app/common/content_visibility.js
@@ -50,6 +50,7 @@ const eu_country_rule      = 'eucountry';
 const options_blocked_rule = 'optionsblocked';
 
 const ContentVisibility = (() => {
+    let $center_select_m;
 
     const init = () => {
         let arr_mt5fin_shortcodes;
@@ -191,7 +192,46 @@ const ContentVisibility = (() => {
         updateTabDisplay();
     };
 
+    // if text is hidden, we need to append it to body to be able to get its width
+    const getTextWidth = (text) => {
+        const $el = $('<span />', { text });
+        $el.prependTo('body');
+        const el_width = $el.width();
+        $el.remove();
+        return el_width;
+    };
+
+    const centerSelect = ($el) => {
+        const option_width = getTextWidth($el.children(':selected').text());
+        const center_option_text = option_width / 2;
+        $el.css('text-indent', `calc(50% - ${center_option_text}px)`);
+    };
+
+    const centerAlignSelect = (should_init) => {
+        $(window).off('resize', centerAlignSelect);
+        $('#frm_real #trading_experience_form select, #frm_real #financial_info_form select').addClass('center-select-m');
+        $center_select_m = ((typeof should_init === 'boolean' && should_init) || !$center_select_m) ? $('#frm_real .center-select-m') : $center_select_m;
+
+        if ($(window).width() <= 480) {
+            const financial_form_selects = $('#frm_real select');
+            financial_form_selects.get().forEach((element) => {
+                const option_width = getTextWidth($(element).children(':selected').text());
+                const center_option_text = option_width / 2;
+                $(element).css('text-indent', `calc(50% - ${center_option_text}px)`);
+            });
+            $center_select_m.on('change', function() {
+                centerSelect($(this));
+            });
+        } else {
+            $center_select_m.each(function() {
+                $(this).css('text-indent', 0);
+            });
+            $(window).resize(centerAlignSelect);
+        }
+    };
+
     return {
+        centerAlignSelect,
         init,
         __test__: {
             parseAttributeString,
